a748ae37239dc28e0caa31cead8087969b978626
alex <shizofranky@gmail.com>
2024-04-15 12:03:29 +0300

    refactor

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,10 @@ import axios from "axios";
 import { resolve } from "path";
 import { Suspense } from "react";
 
+import * as React from 'react';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from '@mui/material/Box';
+
 
 export default async function About() {
   
@@ -14,41 +18,44 @@ export default async function About() {
   return (
     <main className={styles.main}>
       <h1>About</h1>
-      <Suspense fallback="hello world">
-            <p>Lorem ipsum dolor sit amet, consectetur 
-                adipisicing elit. Dignissimos, 
-                neque cumque nostrum quae aspernatur 
-                numquam sunt aliquam optio voluptas incidunt 
-                molestiae illo expedita perspiciatis sequi qui quis, 
-                dolorem eius. Vitae.</p>
-            <Link href={'/'}>to home</Link>
+      <Suspense fallback={<CircularIndeterminate />}>
+            <Slow />
       </Suspense>
         
+            <Link href={'/'}>to home</Link>
     </main>
   );
 }
 
 
-async function getRepos() {
+async function Slow () {
 
-    await new Promise((resolve) => {
-        
-        setTimeout(resolve , 5000 , 'data data data') ;
-        console.log('result result') ;
+  await new Promise((resolve) => {
 
-    });
 
-    
+    setTimeout(resolve , 5000 , 'Jhon Doe')
 
-    const response = await axios.get('https://api.github.com/users/alyxmomsen/repos').catch(err => console.log(err)) ;
-    console.log('result');
-    
-    return <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-    Corporis ea quas perspiciatis sapiente enim aperiam dicta quaerat, 
-    laboriosam praesentium nihil expedita ad voluptates architecto, cumque, 
-    labore natus tempora doloremque consequuntur.</p>
+  } )
 
-    return response ;
 
-    
+  return (
+    <div>
+      slow component:
+      <p>Lorem ipsum dolor sit amet, consectetur 
+                adipisicing elit. Dignissimos, 
+                neque cumque nostrum quae aspernatur 
+                numquam sunt aliquam optio voluptas incidunt 
+                molestiae illo expedita perspiciatis sequi qui quis, 
+                dolorem eius. Vitae.</p>
+    </div>
+  )
+}
+
+
+export function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress />
+    </Box>
+  );
 }
